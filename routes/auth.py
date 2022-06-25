5bfda377c3de1752fa9813c7879e6d4748609a9c
Mateus Xavier <mateus_xa@hotmail.com>
2022-06-25 02:22:19 -0300

    Add error handlers

--- routes/auth.py
+++ routes/auth.py
@@ -1,9 +1,9 @@
-import json
 from operator import and_
-from sqlalchemy import desc, null
+from sqlalchemy.exc import SQLAlchemyError
 import scripts.fii as Fii_Data
 from flask import jsonify, request
 import datetime
+import time
 
 import datetime
 import jwt
@@ -21,18 +21,29 @@ def register():
     email = request.json['email']
     password = request.json['password']
 
-    user = User(
-        username,
-        email,
-        password
-    )
+    try:
+        user = User(
+            username,
+            email,
+            password
+        )
 
-    db.session.add(user)
-    db.session.commit()
+        db.session.add(user)
+        db.session.commit()
 
-    result = user_share_schema.dump(
-        User.query.filter_by(email=email).first()
-    )
+        result = user_share_schema.dump(
+            User.query.filter_by(email=email).first()
+        )
+
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        error = error.lstrip("(").rstrip(")")
+
+        payload = {
+            "errorCode": error[0:4],
+            "errorMessage": error[7:-1]
+        }
+        return jsonify(payload), 400
 
     return jsonify(result)
 
@@ -42,21 +53,50 @@ def login():
     email = request.json['email']
     password = request.json['password']
 
-    user = User.query.filter_by(email=email).first_or_404()
+    for _ in range(3):
+        try:
+            user = User.query.filter_by(email=email).first_or_404()
+        
+            if not user.verify_password(password):
+                return jsonify({
+                    "error": "suas credenciais estao erradas!"
+                }), 403
 
-    if not user.verify_password(password):
-        return jsonify({
-            "error": "suas credenciais estao erradas!"
-        }), 403
+            payload = {
+                "id": user.id,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+            }
 
-    payload = {
-        "id": user.id,
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-    }
+            token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+            
+            return jsonify({"token": token})
+        except SQLAlchemyError as e:
+            error = str(e.__dict__['orig'])
+            error = error.lstrip("(").rstrip(")")
 
-    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
-    
-    return jsonify({"token": token})
+            error_code = error[0:4]
+
+            if error_code == "2013":
+                session_clear()
+                pass
+            else:
+                payload = {
+                    "errorCode": error_code,
+                    "errorMessage": error[7:-1]
+                }
+                return jsonify(payload), 400
+
+    return jsonify({
+        "errorCode": "0000",
+        "errorMessage": "Server may be down"
+    })
+
+
+@app.teardown_request
+def session_clear(exception=None):
+    db.session.remove()
+    if exception and db.session.is_active:
+        db.session.rollback()
 
 
 @app.route('/auth/fiis/all')
